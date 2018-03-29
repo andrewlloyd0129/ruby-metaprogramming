ce12dab42f79f9ed8a8ec0e75bcab8ee913ca92c
andrewlloyd0129 <andrewlloyd0129@gmail.com>
2018-03-29 10:45:23 -0600

    How to Integrate Metaprogramming Techniques Into a Custom Class in Ruby

---
+++ ruby_mp_example_1.rb
@@ -0,0 +1,22 @@
+class Baseball
+end
+
+# p Baseball.new.swing
+
+class Baseball
+  def swing
+    "Homerun"
+  end
+end
+
+# p Baseball.new.swing
+
+
+class Baseball
+  def swing
+    "strike"
+  end
+end
+
+
+p Baseball.new.swing
